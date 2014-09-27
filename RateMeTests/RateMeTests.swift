d6b3cdadd5522782b4f0db6cdd6e25a79aa2c01e
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2014-09-27 12:30:21 -0700

    Initial commit of empty app

---
+++ RateMeTests/RateMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  RateMeTests.swift
+//  RateMeTests
+//
+//  Created by Rescue Mission Software on 9/27/14.
+//  Copyright (c) 2014 Rescue Mission Software. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RateMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
